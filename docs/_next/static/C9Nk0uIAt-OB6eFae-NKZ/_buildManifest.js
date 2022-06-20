bfd67fd46781adbab5355e908575642210b0b4d5
abner.huang <abner.huang@tkspring.com>
2022-06-20 22:39:04 +0800

    docs

--- docs/_next/static/C9Nk0uIAt-OB6eFae-NKZ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/921-2e05b4d75b3d954d.js","static/chunks/pages/index-8e8b672d207671d5.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
