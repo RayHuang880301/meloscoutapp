4578a174bbacadca9851156c7f2373f2f8d59dd8
RayHuang <ray880301@gmail.com>
2022-06-26 00:39:40 +0800

    docs

---
+++ docs/_next/static/d41nmpk__3nHV-cneGrUT/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/921-f9b8cc07d850096a.js","static/chunks/pages/index-79e4ce38e9face4d.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
